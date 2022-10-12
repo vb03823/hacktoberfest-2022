473a08ddfbb9ecebae7f8f8faf827484998bc21b
Muhammad Novrizal Ghiffari <rizal.novrizal999@gmail.com>
2022-10-12 21:11:07 +0700

    create matrix_operation.py

---
+++ matrix_operation.py
@@ -0,0 +1,16 @@
+import numpy
+
+x = numpy.array([[1, 4], [3, 8]])
+y = numpy.array([[10, 9], [7, 13]])
+ 
+# add
+print ("The element wise addition of matrix is : ")
+print (numpy.add(x,y))
+ 
+# subtract
+print ("The element wise subtraction of matrix is : ")
+print (numpy.subtract(x,y))
+ 
+# divide
+print ("The element wise division of matrix is : ")
+print (numpy.divide(x,y))
